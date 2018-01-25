c664e3f4c38cef20fbd2c92f884897937add81cd
yinzhiqiang <yinzhiqiang@medcfc.com>
2018-01-25 18:41:08 +0800

    集成开发中

---
+++ dao/src/main/java/com/quinn/datasource/AppDataSourceConfig.java
@@ -0,0 +1,81 @@
+package com.quinn.datasource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Quinn
+ * @date 2018/1/18
+ * @EnableTransactionManagement 支持开启事物
+ * @package com.quinn.app.datasource
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = AppDataSourceConfig.PACKAGE, sqlSessionFactoryRef = "appSqlSessionFactory")
+public class AppDataSourceConfig {
+
+    // 精确到 master 目录，以便跟其他数据源隔离
+    static final String PACKAGE = "com.quinn.app.dao";
+    static final String MAPPER_LOCATION = "classpath*:mapper/app/*.xml";
+
+
+    @Value("${app.datasource.url}")
+    private String url;
+
+    @Value("${app.datasource.username}")
+    private String user;
+
+    @Value("${app.datasource.password}")
+    private String password;
+
+    @Value("${app.datasource.driver}")
+    private String driverClass;
+
+    @Bean(name = "appDataSource")
+    @Primary
+    public DataSource appDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(name = "appTransactionManager")
+    public DataSourceTransactionManager masterTransactionManager() {
+        return new DataSourceTransactionManager(appDataSource());
+    }
+
+    @Bean(name = "appSqlSessionFactory")
+    public SqlSessionFactory masterSqlSessionFactory(@Qualifier("appDataSource") DataSource masterDataSource)
+            throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(masterDataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(AppDataSourceConfig.MAPPER_LOCATION));
+        return sessionFactory.getObject();
+    }
+
+    /**
+     * 有多少个从库就要配置多少个 application.properties 方式
+     * @return
+     */
+//    @Bean(name = "readDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.read")
+//    public DataSource readDataSourceOne() {
+//        return DataSourceBuilder.create().type(dataSourceType).build();
+//    }
+
+}
