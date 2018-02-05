af40fe6103f3cde9e38014b20ebd3acfbe2cf905
yinzhiqiang <15216880849@qq.com>
2018-02-05 23:11:19 +0800

    测试账号1

--- web/src/main/java/com/quinn/app/Application.java
+++ web/src/main/java/com/quinn/app/Application.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        System.out.println("test");
+        System.out.println("test1");
         return application.sources(Application.class);
     }
     public static void main(String[] args) {
