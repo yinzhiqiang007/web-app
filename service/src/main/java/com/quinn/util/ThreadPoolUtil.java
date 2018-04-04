1df5d88d0a2d49ee17b4318ffa13a6ce60e298d1
yinzhiqiang <yinzhiqiang@medcfc.com>
2018-04-04 14:11:49 +0800

    增加多线程

--- service/src/main/java/com/quinn/util/ThreadPoolUtil.java
+++ service/src/main/java/com/quinn/util/ThreadPoolUtil.java
@@ -1,8 +1,10 @@
 package com.quinn.util;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import com.alibaba.fastjson.JSON;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * @author Quinn
@@ -12,8 +14,46 @@ import java.util.concurrent.ScheduledExecutorService;
 public class ThreadPoolUtil {
 
 
-    public static ExecutorService threadPool = Executors.newFixedThreadPool(5);
+    private static int corePoolSize = 3;
+//    public static ExecutorService threadPool = Executors.newFixedThreadPool(3);
+
+    public static ThreadPoolExecutor threadPool = new ThreadPoolExecutor(corePoolSize, corePoolSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+
+    public static ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(3);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        getThreadPoolInfo(threadPool);
+        for (int i = 0; i < 20; i++) {
+            getThreadPoolInfo(threadPool);
+        }
+        Thread.sleep(10000);
+        getThreadPoolInfo(threadPool);
 
 
-    public static ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+    }
+
+    public static Map<String, Object> getThreadPoolInfo(ThreadPoolExecutor threadPool) {
+        Map<String, Object> threadPoolMap = new HashMap<>();
+        threadPoolMap.put("getPoolSize", threadPool.getPoolSize());
+        threadPoolMap.put("getTaskCount", threadPool.getTaskCount());
+        threadPoolMap.put("isShutdown", threadPool.isShutdown());
+        threadPoolMap.put("getCorePoolSize", threadPool.getCorePoolSize());
+        threadPoolMap.put("getMaximumPoolSize", threadPool.getMaximumPoolSize());
+        threadPoolMap.put("getQueue_size", threadPool.getQueue().size());
+        threadPoolMap.put("getActiveCount", threadPool.getActiveCount());
+        threadPoolMap.put("getCompletedTaskCount", threadPool.getCompletedTaskCount());
+        threadPoolMap.put("getLargestPoolSize", threadPool.getLargestPoolSize());
+        System.out.println(JSON.toJSONString(threadPoolMap));
+        return threadPoolMap;
+    }
+
+    public static Map<String, Object> getThreadPoolInfo() {
+        Map<String, Object> m1 = getThreadPoolInfo(threadPool);
+        Map<String, Object> all = new HashMap<>();
+        all.put("threadPool", m1);
+        return all;
+    }
+
+
 }
