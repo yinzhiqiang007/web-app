b7d8b99ef791c96d81b916277a4f3b76f7e22cdc
yinzhiqiang <yin.zhiqiang@xiaoyi.com>
2018-08-20 17:46:30 +0800

    测试3

--- web/src/main/java/com/quinn/app/controller/UserController.java
+++ web/src/main/java/com/quinn/app/controller/UserController.java
@@ -55,6 +55,7 @@ public class UserController {
     public Map<String,Object> test(String id){
         System.out.println("1111");
         System.out.println("2222");
+        System.out.println("3333");
         systemConfigRedisService.setNX("dddd","sd水电费水电费");
         systemConfigRedisService.put("hehehehe","ttttt",200);
         Map<String,Object> resultMap = new HashMap();
