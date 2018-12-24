973f73510f9894b436f6e1b14da1ff5c86736535
yin.zhiqiang <a2051896>
2018-12-24 16:19:40 +0800

    系统维护

--- service/src/main/java/com/quinn/scheduler/AbstractExecutor.java
+++ service/src/main/java/com/quinn/scheduler/AbstractExecutor.java
@@ -2,20 +2,41 @@ package com.quinn.scheduler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class AbstractExecutor implements Executor {
 
-    public static Map<Integer,Long> schedulerMap = new HashMap<>();
+    public static Map<Integer, Long> schedulerMap = new HashMap<>();
+
     static {
-        schedulerMap.put(1,1l);
-        schedulerMap.put(2,2l);
-        schedulerMap.put(3,8l);
-        schedulerMap.put(4,30l);
-        schedulerMap.put(5,120l);
-        schedulerMap.put(6,1200l);
-        schedulerMap.put(7,1800l);
-        schedulerMap.put(8,3600l);
+        schedulerMap.put(1, 1l);
+        schedulerMap.put(2, 2l);
+        schedulerMap.put(3, 8l);
+        schedulerMap.put(4, 30l);
+        schedulerMap.put(5, 120l);
+        schedulerMap.put(6, 1200l);
+        schedulerMap.put(7, 1800l);
+        schedulerMap.put(8, 3600l);
 //        schedulerMap.put(9,12000l);
 //        schedulerMap.put(10,86400l);
     }
+
+    /**
+     * 第一次请求
+     */
+    public static final int first_invoke = 1;
+
+    /*** 责任链起始位置 */
+    public static int chain_index = 0;
+
+    /**
+     * 存放链条每个节点结果，用户下一个链条使用
+     */
+    public static Map<String, Object> concurrentHashMap = new ConcurrentHashMap<>();
+
+
+    /**
+     * 注册链
+     */
+    public static final String[] user_chain = new String[]{"userExecutor"};
 }
--- service/src/main/java/com/quinn/scheduler/Scheduler.java
+++ service/src/main/java/com/quinn/scheduler/Scheduler.java
@@ -2,6 +2,7 @@ package com.quinn.scheduler;
 
 import com.alibaba.fastjson.JSON;
 import com.quinn.app.common.constans.ResponseEnum;
+import com.quinn.app.common.util.CommonUtils;
 import com.quinn.app.common.util.LogUtils;
 import com.quinn.util.SpringUtil;
 import com.quinn.util.ThreadPoolUtil;
@@ -11,29 +12,52 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class Scheduler implements Callable {
-    private String beanName;
-    private Integer count;
-    private Map<String, Object> map;
+    private int count;//执行次数
+    private Map<String, Object> map;//参数
+    private int chainIndex;//被执行责任链的当前位置
+    private String[] beanChain;//被执行责任链
 
-    public Scheduler(String beanName, Integer count, Map<String, Object> map) {
-        this.beanName = beanName;
+    public Scheduler(int count, Map<String, Object> map, int chainIndex, String[] beanChain) {
         this.count = count;
         this.map = map;
+        this.chainIndex = chainIndex;
+        this.beanChain = beanChain;
+
+    }
+
+    public Scheduler(Map<String, Object> map,String[] beanChain) {
+        this.count = AbstractExecutor.first_invoke;
+        this.map = map;
+        this.chainIndex = AbstractExecutor.chain_index;
+        this.beanChain = beanChain;
 
     }
 
     @Override
     public Object call() throws Exception {
+        String beanName = beanChain[chainIndex];
+        if (count == AbstractExecutor.first_invoke) {
+            map.put("chainId", CommonUtils.getUUIDString());
+        }
         Executor handler = (Executor) SpringUtil.getBean(beanName);
         boolean b = handler.invoke(map);
         if (!b) {
-            LogUtils.error(ResponseEnum.code_999999.name(), "执行器执行返回异常...count:" + count);
+            LogUtils.error(ResponseEnum.code_999999.name(), "执行器执行返回异常..:" + JSON.toJSONString(map));
             count++;
             Long c = AbstractExecutor.schedulerMap.get(count);
             if (c != null) {
-                ThreadPoolUtil.scheduledThreadPool.schedule(new Scheduler(beanName, count, map), c, TimeUnit.SECONDS);
+                ThreadPoolUtil.scheduledThreadPool.schedule(new Scheduler(count, map, chainIndex, beanChain), c, TimeUnit.SECONDS);
             } else {
+                AbstractExecutor.concurrentHashMap.remove(map.get("chainId").toString());
                 LogUtils.error(ResponseEnum.code_999999.name(), "执行器执行返回异常，超出最大执行次数count:" + count + "bean:" + beanName + "param:" + JSON.toJSONString(map));
+            }
+        } else {
+            chainIndex++;
+            if (chainIndex < beanChain.length) {
+                ThreadPoolUtil.threadPool.submit(new Scheduler(count, map, chainIndex, beanChain));
+            }else{
+                AbstractExecutor.concurrentHashMap.remove(map.get("chainId").toString());
+                LogUtils.info(ResponseEnum.code_000000.name(), "执行器执行完毕，bean:" + beanName + "param:" + JSON.toJSONString(map));
             }
         }
         return "ok";
