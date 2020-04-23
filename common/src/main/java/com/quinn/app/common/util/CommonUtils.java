6b4777e8ee4e525920f83d9029eb358211e801f6
yin.zhiqiang <yin.zhiqiang@xiaoyi.com>
2020-04-23 16:28:33 +0800

    升级fast json版本

--- common/src/main/java/com/quinn/app/common/util/CommonUtils.java
+++ common/src/main/java/com/quinn/app/common/util/CommonUtils.java
@@ -1,11 +1,14 @@
 package com.quinn.app.common.util;
 
+
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.quinn.app.common.constans.BaseEnum;
 
 /**
  * 判断工具包
@@ -96,6 +99,34 @@ public class CommonUtils {
     }
 
     /**
+     * 返回long类型-最小-秒
+     *
+     * @param v
+     * @return
+     */
+    public static Long getLongBySeconds(Long v) {
+        if (v.toString().length() < 13) {
+            return v;
+        } else {
+            return v / 1000;
+        }
+    }
+
+    /**
+     * 返回long类型-最小-毫秒
+     *
+     * @param v
+     * @return
+     */
+    public static Long getLongByMillisecond(Long v) {
+        if (v.toString().length() >= 13) {
+            return v;
+        } else {
+            return v * 1000;
+        }
+    }
+
+    /**
      * 判断是否是可用枚举
      **/
     public static <T extends Enum<T>> boolean isEnum(Class<T> c, String name) {
@@ -107,35 +138,65 @@ public class CommonUtils {
         }
     }
 
-    /**
-     * 判断是否是可用枚举
-     **/
-    public static boolean isEnum(BaseEnum[] bb, String name) {
-        for (BaseEnum b : bb) {
-            if (b.name().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-
-    /**
-     * 判断是否是可用枚举
-     **/
-    public static boolean isEnum(BaseEnum[] bb, Integer code) {
-        for (BaseEnum b : bb) {
-            if (b.getCode().equals(code)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     public static String getStringTime(Date date, String patter) {
         SimpleDateFormat format = new SimpleDateFormat(patter);
         return format.format(date);
     }
+
+    public static String valueOfString(Object o) {
+        if (isNotBlank(o)) {
+            return o.toString();
+        } else {
+            return "";
+        }
+    }
+
+    public static String valueOfNull(Object o) {
+        if (isNotBlank(o)) {
+            return o.toString();
+        } else {
+            return null;
+        }
+    }
+
+    public static LocalDateTime longToLocalDateTime(long t) {
+        Instant instant = Instant.ofEpochMilli(t);
+        ZoneId zone = ZoneId.systemDefault();
+        return LocalDateTime.ofInstant(instant, zone);
+    }
+
+    public static Long localDateTimeToLong(LocalDateTime localDateTime) {
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDateTime.atZone(zone).toInstant();
+        return instant.toEpochMilli();
+    }
+
+    public static boolean isDateFormat(String format){
+        for (DATE_FORMAT b : DATE_FORMAT.values()) {
+            if (b.getFormat().equals(format)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String longToDateStr(String format,Long time){
+        if(!isDateFormat(format)){
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return formatter.format(longToLocalDateTime(time));
+    }
+
+    public static String toDateStr(String format,LocalDateTime time){
+        if(!isDateFormat(format)){
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return formatter.format(time);
+    }
+
 
     public static String getUUIDString() {
         return UUID.randomUUID().toString().replace("-", "");
@@ -154,10 +215,34 @@ public class CommonUtils {
         }
     }
 
-
-
-    public static void main(String[] args) {
-        System.out.println(isBigDecimal("23423.234324"));
+    public static <T> T[] toArray(T... items) {
+        return items;
     }
+
+    public enum DATE_FORMAT{
+        FORMAT1("yyyy-MM-dd"),
+        FORMAT2("yyyy-MM-dd HH:mm:ss");
+
+        private String format;
+        DATE_FORMAT(String format){
+            this.format = format;
+        }
+        public String getFormat() {
+            return format;
+        }
+
+        public String getFormat(Long time) {
+            return longToDateStr(format,time);
+        }
+
+        public String getFormat(LocalDateTime time) {
+            return toDateStr(format,time);
+        }
+
+        public void setFormat(String format) {
+            this.format = format;
+        }
+    }
+
 
 }
